6904ba96997665e07066d1972bdc746e0dbd8b0f
futurism-akhil <akhils@futurismtechnologies.com>
2014-08-15 02:41:53 +0530

    formatting updated

--- js/app/main.js
+++ js/app/main.js
@@ -1,13 +1,13 @@
 viewService = (function ($) {
     var container = '.container-fluid';
-    var row = 0, col = 0;
+    var row = 1, col = -1;
     var rowTemplate = function (row) {
         return '<div class="row" id=row' + row + '></div>';
     };
     var btnTemplate = function (activeRow, activeCol) {
         activeRow = activeRow || row;
         activeCol = activeCol || (col = col + 1);
-        return '<button class="sqr" id=col' + activeRow + activeCol + '></button>';
+        return '<button class="sqr" id=col' + activeRow + activeCol + ' item="itemx"></button>';
     };
 
     return {
@@ -16,14 +16,23 @@ viewService = (function ($) {
             activeRow = activeRow || row;
             activeCol = activeCol || (col = col + 1);
 
-            var rowDiv = $('#row' + activeRow)
+            var rowDiv = $('#row' + activeRow);
             if (rowDiv.length == 0) {
+                activeCol = 0;
+                col = 0;
+                row = activeCol;
                 rowDiv = $(rowTemplate(activeRow))
                     .appendTo(container);       // creating new row
                 //rowDiv = $('#row'+activeRow)
             }
+            var btn = $('#col' + activeRow + activeCol);
+            if (btn.length == 0) {
+                btn = $(btnTemplate(activeRow, activeCol)).appendTo(rowDiv);
+            }    // adding a button to selected row
 
-            $(btnTemplate()).appendTo(rowDiv);    // adding a button to selected row
+
+            return btn;
+
         }
     }
 })(jQuery);
