e43e735bfc065af6137145dcc2037521443d68d9
futurism-akhil <akhils@futurismtechnologies.com>
2014-08-18 22:50:22 +0530

    formatting updated

--- js/app/main.js
+++ js/app/main.js
@@ -1,4 +1,4 @@
-viewService = (function ($) {
+var viewService = (function ($) {
     var container = '.container-fluid';
     var row = 1, col = -1;
     var rowTemplate = function (row) {
@@ -10,6 +10,9 @@ viewService = (function ($) {
         return '<button class="sqr" id=col' + activeRow + activeCol + ' item="itemx"></button>';
     };
 
+    var msgTemp = function (msg) {
+        return '<p class="orngMsg">' + msg + '</p>';
+    };
     return {
 
         addButton: function (activeRow, activeCol) {
@@ -20,7 +23,7 @@ viewService = (function ($) {
             if (rowDiv.length == 0) {
                 activeCol = 0;
                 col = 0;
-                row = activeCol;
+                row = activeRow;
                 rowDiv = $(rowTemplate(activeRow))
                     .appendTo(container);       // creating new row
                 //rowDiv = $('#row'+activeRow)
@@ -30,27 +33,73 @@ viewService = (function ($) {
                 btn = $(btnTemplate(activeRow, activeCol)).appendTo(rowDiv);
             }    // adding a button to selected row
 
-
             return btn;
+        }, showMsg: function (msg, timeOut) {
+            timeOut = timeOut || 2000;
+            $('.container-fluid').first().after(msgTemp(msg));
+            setTimeout(function () {
+                $('.orngMsg').remove();
+            }, timeOut);
+        }, drawGame: function (elemt) {
+            var columns = 3, activeRow = 1;
+            var container = $('.container-fluid');
+            //   container.css('display','none');
+            while (elemt > 0) {
+                for (var col = 0; col < columns; col++) {
+                    debugger;
+                    this.addButton(activeRow);
+                }
+                elemt--;
+                activeRow++;
+            }
 
+            //  container.css('display','');
+        }
+    }
+})(jQuery);
+
+var dataService = (function ($) {
+    var url = 'data/data.json';
+    var dataCache = [];
+
+    var init = function () {
+        $.ajax({url: url, async: false})
+            .done(function (data) {
+                dataCache = data;
+            })
+            .fail(function (jqXHR, textStatus) {
+                alert("Request failed: " + textStatus);
+                dataCache = [];
+            });
+    };
+
+    init();
+
+    return {
+        loadData: function () {
+            init();
+            return  dataCache;
+        }, getData: function () {
+            return  dataCache;
+        }, getRandomInt: function (min, max) {
+            return Math.floor(Math.random() * (max - min)) + min;
         }
     }
 })(jQuery);
 
 
-/*var app = (
- function(){
+var app = (function (view, data) {
+    //http://www.placehold.it/350x160
+    //http://www.placehold.it/350x150/fffxff/ffffff&text=akhil
+    var no = 2 * data.getRandomInt(6, 12);
+
+    return {
+        init: function () {
+            view.drawGame(no);
+        }
+    }
+})(viewService, dataService);
 
 
-
- function init(){
- alert('loaded');
- }
-
- return {
- init : init
- }
- }
- )();*/
-
+app.init();
 //debugger;
