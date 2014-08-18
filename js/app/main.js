94ac9b5dd92d9d806691c5cd49bf3f252d29cecc
futurism-akhil <akhils@futurismtechnologies.com>
2014-08-19 01:25:25 +0530

    formatting updated

--- js/app/main.js
+++ js/app/main.js
@@ -1,59 +1,58 @@
 var viewService = (function ($) {
     var container = '.container-fluid';
-    var row = 1, col = -1;
+    var row = 1, col = 1;
     var rowTemplate = function (row) {
         return '<div class="row" id=row' + row + '></div>';
     };
-    var btnTemplate = function (activeRow, activeCol) {
+    var btnTemplate = function (activeRow, activeCol, msg, tag) {
         activeRow = activeRow || row;
         activeCol = activeCol || (col = col + 1);
-        return '<button class="sqr" id=col' + activeRow + activeCol + ' item="itemx"></button>';
+        return '<button class="sqr" id=col' + activeRow + activeCol + ' item="' + tag + '">' + msg + '</button>';
     };
 
     var msgTemp = function (msg) {
         return '<p class="orngMsg">' + msg + '</p>';
     };
     return {
-
-        addButton: function (activeRow, activeCol) {
+        addButton: function (activeRow, activeCol, tag, handler) {
             activeRow = activeRow || row;
             activeCol = activeCol || (col = col + 1);
 
             var rowDiv = $('#row' + activeRow);
             if (rowDiv.length == 0) {
-                activeCol = 0;
-                col = 0;
+                activeCol = 1;
+                col = 1;
                 row = activeRow;
-                rowDiv = $(rowTemplate(activeRow))
-                    .appendTo(container);       // creating new row
+                rowDiv = $(rowTemplate(activeRow)).appendTo(container);       // creating new row
                 //rowDiv = $('#row'+activeRow)
             }
             var btn = $('#col' + activeRow + activeCol);
             if (btn.length == 0) {
-                btn = $(btnTemplate(activeRow, activeCol)).appendTo(rowDiv);
+                btn = $(btnTemplate(activeRow, activeCol, [activeRow, activeCol].join('/'), tag)).appendTo(rowDiv);
             }    // adding a button to selected row
-
+            btn.on('click', handler);
             return btn;
-        }, showMsg: function (msg, timeOut) {
+        },
+        showMsg: function (msg, timeOut) {
             timeOut = timeOut || 2000;
             $('.container-fluid').first().after(msgTemp(msg));
             setTimeout(function () {
                 $('.orngMsg').remove();
             }, timeOut);
-        }, drawGame: function (elemt) {
+        },
+        drawGame: function (elemt, handler) {
             var columns = 3, activeRow = 1;
             var container = $('.container-fluid');
-            //   container.css('display','none');
-            while (elemt > 0) {
+            container.css('display', 'none');
+            var lng = elemt.length;
+            while (lng > 0) {
                 for (var col = 0; col < columns; col++) {
-                    debugger;
-                    this.addButton(activeRow);
+                    this.addButton(activeRow, col + 1, elemt[lng - 1].name, handler);
+                    lng--;
                 }
-                elemt--;
                 activeRow++;
             }
-
-            //  container.css('display','');
+            container.css('display', '');
         }
     }
 })(jQuery);
@@ -79,10 +78,17 @@ var dataService = (function ($) {
         loadData: function () {
             init();
             return  dataCache;
-        }, getData: function () {
+        }, getData: function (no) {
             return  dataCache;
+        }, getDataN: function (no) {
+            var slice = dataCache.slice(0, no);
+            return  slice.concat(dataCache.slice(0, no));
         }, getRandomInt: function (min, max) {
             return Math.floor(Math.random() * (max - min)) + min;
+        }, shuffleEl: function (data) {
+            return data.sort(function () {
+                return .5 - Math.random();
+            });
         }
     }
 })(jQuery);
@@ -91,15 +97,19 @@ var dataService = (function ($) {
 var app = (function (view, data) {
     //http://www.placehold.it/350x160
     //http://www.placehold.it/350x150/fffxff/ffffff&text=akhil
-    var no = 2 * data.getRandomInt(6, 12);
-
+    var no = data.getRandomInt(6, 12);
+    var nEnteries = data.shuffleEl(data.getDataN(no));
+    var selectedIndex = -1;
     return {
         init: function () {
-            view.drawGame(no);
+            view.drawGame(nEnteries, this.handler);
+        }, handler: function (event) {
+
+            if (selectedIndex >= -1) {
+                var selectItem = this.getAttribute('item');
+            }
+
+            alert(selectItem);
         }
     }
 })(viewService, dataService);
-
-
-app.init();
-//debugger;
